f445e0f598cb41e665bbc106e045499d7edbc3bd
Shuhao1228 <1095280484@qq.com>
2019-08-24 16:31:28 +0800

    fixed tabbarStyle

--- src/views/Cart/Cart.js
+++ src/views/Cart/Cart.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import css from './Cart.module.scss'
+import {NavLink} from 'react-router-dom' //Link  Navlink
 
 class Cart extends Component {
     state = {  }
     render() {
         return (
-            <div>Cart</div>            
+            <section className="no-result">Cart
+                {/* <NavLink to="/user" activeClassName="button-yellow">登陆</NavLink> */}
+            </section>            
         );
     }
 }
